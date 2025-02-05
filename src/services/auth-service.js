02ca1728b13a95423288117620281d7ed623c33d
IvannIvannov <ivann.ivannov26@icloud.com>
2025-02-05 13:40:38 +0200

    add cast validations and user validations

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -5,7 +5,16 @@ import jwt from 'jsonwebtoken';
 const SECRET = process.env.JWT_SECRET || 'BASICSECRET';
 
 export default {
-    register(userData) {
+    async register(userData) {
+        if(userData.password !== userData.rePassword){
+            throw new Error('Password missmatch!');
+        }
+
+        const userCount = await User.countDocuments({email: userData.email});
+        if(userCount > 0){
+            throw new Error('Email already exists');
+        }
+
         return User.create(userData);
     },
     async login(email, password) {
